dd45edded212a764fdf242dd89abee9a6a8fd9f1
Leonardo Wiest <leonardowiest@hotmail.com>
2020-01-24 18:37:07 -0300

    Doc: Configurações de conexão com Postgres

--- monitor-api/src/main/java/com/github/leonardowiest/Application.java
+++ monitor-api/src/main/java/com/github/leonardowiest/Application.java
@@ -2,10 +2,8 @@ package com.github.leonardowiest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication()
-@PropertySource(ignoreResourceNotFound = false, value = "classpath:application-${spring.profiles.active}.properties")
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
