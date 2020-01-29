1b9744526e435c64922e80d73d8578b458856f43
Leonardo Wiest <leonardowiest@hotmail.com>
2020-01-29 20:20:15 -0300

    Doc: Sincronização

--- integrador-service/src/main/java/com/github/leonardowiest/factory/impl/SincronizacaoPrimariaFactoryImpl.java
+++ integrador-service/src/main/java/com/github/leonardowiest/factory/impl/SincronizacaoPrimariaFactoryImpl.java
@@ -1,8 +1,5 @@
 package com.github.leonardowiest.factory.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.github.leonardowiest.factory.SincronizacaoPrimariaFactory;
 
 public class SincronizacaoPrimariaFactoryImpl implements SincronizacaoPrimariaFactory {
@@ -10,14 +7,7 @@ public class SincronizacaoPrimariaFactoryImpl implements SincronizacaoPrimariaFa
 	@Override
 	public Boolean sincronizar() {
 
-		List<?> sincronizacoesDesatualizadas = buscarSincronizacoesDesatualizadas();
-
 		return Boolean.TRUE;
-	}
-
-	private List<?> buscarSincronizacoesDesatualizadas() {
-		
-		return new ArrayList<>();
 	}
 
 }
